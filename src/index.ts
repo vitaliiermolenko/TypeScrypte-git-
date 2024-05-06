0725f3e19830d1b50167eaab7706094996684591
vitaliiermolenko <vetik04@ukr.net>
2024-05-06 16:11:06 +0200

    hm8

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const msg: string = "hi";
-console.log(msg);
+class Movie {
+    constructor(public title: string, public year: number, public rating: number, public awards: string[]) {}
+}
+
+class MovieCategory {
+    constructor(public name: string, public movies: Movie[]) {}
+}
+
+type FilterState = {
+    matchingFilters?: { filter: string }[],
+    rangeFilters?: { filter: number, filterTo: number }[],
+    valuesSearchFilters?: { values: string[] }[]
+};
