767803750f7e71f7dac98a49012dcca4f6a7d8cf
vitaliiermolenko <vetik04@ukr.net>
2024-05-07 19:10:10 +0200

    hm8_one_entity

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-class Movie {
-    constructor(public title: string, public year: number, public rating: number, public awards: string[]) {}
+interface Entity {
+    name: string;
 }
 
-class MovieCategory {
-    constructor(public name: string, public movies: Movie[]) {}
+interface Movie extends Entity {
+    year: number;
+    rating: number;
+    awards: string[];
 }
 
-type FilterState = {
-    matchingFilters?: { filter: string }[],
-    rangeFilters?: { filter: number, filterTo: number }[],
-    valuesSearchFilters?: { values: string[] }[]
-};
+interface Category<T extends Entity> extends Entity {
+    items: T[];
+}
+
+interface Filter<T> {
+    type: 'search' | 'range' | 'values';
+    filter?: keyof T;
+    filterTo?: T[keyof T];
+    values?: T[keyof T][];
+}
+
+class List<T extends Entity> {
+    items: T[];
+    filters: Filter<T>[];
+
+    constructor(items: T[], filters: Filter<T>[]) {
+        this.items = items;
+        this.filters = filters;
+    }
+
+    applySearchFilter(filter: Filter<T>) {
+        if (filter.type === 'search' && filter.filter) {
+            // Implement search logic based on filter.filter
+        }
+    }
+
+    applyRangeFilter(filter: Filter<T>) {
+        if (filter.type === 'range' && filter.filter && filter.filterTo) {
+            // Implement range filter logic based on filter.filter and filter.filterTo
+        }
+    }
+
+    applyValuesFilter(filter: Filter<T>) {
+        if (filter.type === 'values' && filter.values) {
+            // Implement values filter logic based on filter.values
+        }
+    }
+}
